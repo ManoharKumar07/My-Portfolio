6c3852725ae0be17b841e44e14f7a2fc68cc3463
Manohar Kumar <manohar.csengineer@gmail.com>
2024-10-03 23:41:43 +0530

    nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import Nav from "./Nav";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white ">
-      <div className="container mx-auto ">
+      <div className="container mx-auto flex justify-between items-center ">
         <Link href="/">
           <h1 className="text-4xl font-semibold">
             Manohar Kumar <span className="text-accent">.</span>
           </h1>
         </Link>
+
+        {/* Computer Nav */}
+        <div className="hidden xl:flex items-center gap-8 font-semibold">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+
+        {/* Mobile Nav */}
+        <div className="xl:hidden">Mobile nav</div>
       </div>
     </header>
   );
