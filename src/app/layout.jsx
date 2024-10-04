808275f637694da2d50992163a0850ab62f33b45
Manohar Kumar <manohar.csengineer@gmail.com>
2024-10-04 22:02:31 +0530

    Stair Transition for page change

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,7 @@ import { JetBrains_Mono } from "next/font/google";
 //Components
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -40,7 +41,7 @@ export default function RootLayout({ children }) {
         `}
       >
         <Header />
-
+        <StairTransition />
         <PageTransition>{children}</PageTransition>
       </body>
     </html>
